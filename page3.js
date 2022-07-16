37ca08c568da5561e0d573bf730b2b1a723fc4a9
codewithoutload <codewithoutload@gmail.com>
2022-07-17 00:19:46 +0530

    third commit for location javascript

--- page3.js
+++ page3.js
@@ -25,7 +25,7 @@ var num2=position.coords.longitude;
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://us1.locationiq.com/v1/reverse?key=pk.bfec039abdf82022192ea39cf6935d75&lat=num1&lon=num2&format=json",
+    "url": "https://us1.locationiq.com/v1/reverse?key=pk.c44ad4ffbe43c0140dd4a83af0f07ba6&lat=num1&lon=num2&format=json",
     "method": "GET"
   }
   
